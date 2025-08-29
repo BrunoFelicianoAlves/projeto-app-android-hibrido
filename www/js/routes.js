6cfcf177f7be8dd159fbc3d6531ae0f0d4e50fdf
Bruno Feliciano Alves <bruno.felicianobh@gmail.com>
2025-08-29 14:18:03 -0300

    Adicionado produtos

--- www/js/routes.js
+++ www/js/routes.js
@@ -55,21 +55,29 @@ var app = new Framework7({
             spaceBetween: 30
           }
         }
-        // freeMode: true,
-        // pagination: {
-        //   el: ".swiper-pagination",
-        //   clickable: true,
-        // },
     });
 
-    var swiper = new Swiper(".categorias", {
+    var swiper2 = new Swiper(".categorias", {
         slidesPerView: 3,
         spaceBetween: 10,
-        freeMode: true,
-        // pagination: {
-        //   el: ".swiper-pagination",
-        //   clickable: true,
-        // },
+        breakpoints: {
+          50: {
+            slidesPerView: 3,
+            spaceBetween: 30
+          },
+          640: {
+            slidesPerView: 3,
+            spaceBetween: 30
+          },
+          992: {
+            slidesPerView: 3,
+            spaceBetween: 30
+          },
+          1200: {
+            slidesPerView: 4,
+            spaceBetween: 30
+          }
+        }
       });
 		},
 		pageBeforeRemove: function (event, page) {
